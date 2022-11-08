0b99525963062e71cb139c413e09cf123ceecdf2
AbdulazizYas <abdulazizyass@gmail.com>
2022-11-08 20:48:16 +0300

    test

--- msaaq/js/script.js
+++ msaaq/js/script.js
@@ -1,8 +1,8 @@
-document.onload = function () {
+setTimeout(function () {
   const img = document.querySelector("main .col .card img");
 
   const cardbody = img.nextElementSibling;
 
   img.remove();
   cardbody.append(img);
-};
+}, 1000);
